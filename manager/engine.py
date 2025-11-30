6c1b8acde21ffb8e8a455c5e3ecf405da02c4386
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-30 12:22:33 +0700

    Fix GameEngine initialization and implement start_game method for proper game setup

--- manager/engine.py
+++ manager/engine.py
@@ -1,10 +1,14 @@
 from view.view import init
+from models.world import World
 
 class GameEngine:
+    def __init__(self):
+        self.world = World()
 
-    def start_game():
-        while True:
-            init()
+    def start_game(self):
+        self.world.generate_world()
+
+
             
             
             
