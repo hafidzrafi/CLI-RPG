882afc5dd94e18e27dcaaa1382b9a58357fd8026
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-11-30 12:02:12 +0700

    Add predefined Room instances and connections to enhance game world structure

--- models/world.py
+++ models/world.py
@@ -9,3 +9,21 @@ class Room:
     def connects(self, direction, next_room):
         self.exits[direction] = next_room
 
+cloud = Room("Cloud Association", "Association of Players from All Over of The World")
+orthodox = Room("Orthodox Alliance", "The Central Headquarters of Murim Orthodox")
+unorthodox = Room("Unorthodox Alliance", "The Alliance of Unorthodox Murim Martial Artist")
+demonic = Room("Heavenly Demonic Cult", "A Cult Founded by Cheon Ma, The First Heavenly Demon Emperor")
+ice = Room("North Sea Ice Palace", "A palace located in the North Sea whose main attribute is ice")
+
+cloud.connects("west", orthodox)
+
+orthodox.connects("north", ice)
+orthodox.connects("south", demonic)
+orthodox.connects("west", cloud)
+orthodox.connects("east", unorthodox)
+
+unorthodox.connects("east", orthodox)
+
+demonic.connects("north", orthodox)
+
+ice.connects("south", orthodox)
