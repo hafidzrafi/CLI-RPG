00d0d661d999c481d1f80e87cfdeb59032673d00
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2025-12-07 21:52:24 +0700

    style(config): apply black formatting and update headers
    Compressed dictionary definitions to single lines via Black. Renamed section header from 'Konstanta umum' to 'General Constant'.

--- config.py
+++ config.py
@@ -1,43 +1,18 @@
-# Konstanta umum
+# General Constant
 SAVE_FILE_NAME = "data.json"
 INITIAL_ROOM_KEY = "cloud"
 
 # Data Player
 PLAYER = {
-    "FIGHTER"   : {
-        "HP"    : 200,
-        "DAMAGE": 60
-    },
-    "ARCHER"    : {
-        "HP"    : 120,
-        "DAMAGE": 100
-    },
-    "TANK"      : {
-        "HP"    : 500,
-        "DAMAGE": 24
-    }
+    "FIGHTER": {"HP": 200, "DAMAGE": 60},
+    "ARCHER": {"HP": 120, "DAMAGE": 100},
+    "TANK": {"HP": 500, "DAMAGE": 24},
 }
 
 # Data Monsters
 MONSTERS = {
-    "GOBLIN"    : {
-        "NAME"  : "Goblin",
-        "HP"    : 200,
-        "DAMAGE": 50
-    },
-    "SKELETON"  : {
-        "NAME"  : "Skeleton",
-        "HP"    : 350,
-        "DAMAGE": 75
-    },
-    "MINOTAUR"  : {
-        "NAME"  : "Minotaur",
-        "HP"    : 500,
-        "DAMAGE": 300
-    },
-    "DRAGON"    : {
-        "NAME"  : "Dragon",
-        "HP"    : 9999,
-        "DAMAGE": 999
-    }
+    "GOBLIN": {"NAME": "Goblin", "HP": 200, "DAMAGE": 50},
+    "SKELETON": {"NAME": "Skeleton", "HP": 350, "DAMAGE": 75},
+    "MINOTAUR": {"NAME": "Minotaur", "HP": 500, "DAMAGE": 300},
+    "DRAGON": {"NAME": "Dragon", "HP": 9999, "DAMAGE": 999},
 }
